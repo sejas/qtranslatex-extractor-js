e882de309a8e094c48069727aad37c1937cbff53
Sejas <antonio@sejas.es>
2019-02-23 17:22:37 +0000

    updated tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,9 +1,17 @@
-import qExtractor from '../index';
+import qExtractor from '../index'
 
 const demoContent = `[:en]Discover our range of vegetable tanned leather wallets.
 Handcrafted in Spain by the best artisans.
 Europe & Worldwide Delivery. Free shipping orders over 150€.[:es]Descubre nuestras carteras de piel Premium hechas a mano en Ubrique.
-Envío gratis en España en pedidos superiores a 60€.[:]`;
-test('Dummy test', () => {
-  expect(true).toBe(true);
-});
+Envío gratis en España en pedidos superiores a 60€.[:]`
+
+test('Check length extracted equal to 2', () => {
+  const contentExtracted1 = qExtractor(demoContent)
+  expect(Object.keys(contentExtracted1).length).toBe(2)
+})
+
+test('Check languages extracted ["en", "es"]', () => {
+  const contentExtracted2 = qExtractor(demoContent)
+  expect(Object.keys(contentExtracted2)[0]).toBe('en')
+  expect(Object.keys(contentExtracted2)[1]).toBe('es')
+})
