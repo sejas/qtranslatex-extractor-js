cf61e0e5464afde6c3b4cadf2380ecef51863d92
Sejas <antonio@sejas.es>
2019-02-23 17:23:48 +0000

    clean code before publish v0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 const qExtractor = (contentInMultipleLanguages: string): object => {
-  //   return contentInMultipleLanguages.split(/\[:.*\]/gm);
   const reg1 = /\[:[\S]*\]/gm
   const reg2 = /\[:[\S]*\]/gm
   const content = contentInMultipleLanguages.split(reg1).filter(t => '' !== t)
   let langs = contentInMultipleLanguages.match(reg2)
   langs = langs && langs.slice(0, -1).map(l => l.replace(/\[|:|\]/g, ''))
-  //   console.log({ langs, content })
   if (langs && langs.length === content.length) {
     const result: any = {}
     langs.forEach((lang, i) => {
-      //   console.log(lang)
       result[lang] = content[i]
     })
     return result
